3dd4ec0141c5ace5f15ab5162cad6aaf7172fb01
Markus <>
2021-02-08 17:04:57 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <Gif :msg="message" />
+  </div>
+</template>
+
+<script>
+import Gif from "./components/Gif.vue";
+export default {
+  components: { Gif },
+  beforeMount() {
+    this.created();
+  },
+  name: "App",
+  data() {
+    return {
+      message: "Using Parcel In A Vue.js App",
+    };
+  },
+  methods: {
+    created() {
+      this.message = process.env.NODE_ENV;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 600px;
+  margin: 50px auto;
+  text-align: center;
+}
+</style>
---
+++ src/components/Gif.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="isDev()">
+    <div class="dev"></div>
+    <h1>dev mode</h1>
+  </div>
+  <div v-else>
+    <img src="" alt="Kitten" />
+    <h1>prod mode</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gif",
+  methods: {
+    isDev() {
+      return process.env.NODE_ENV === "dfs";
+    },
+  },
+  mounted() {
+    let myImage = document.querySelector("img");
+
+    fetch("https://placekitten.com/100/100")
+      .then((response) => response.blob())
+      .then((myBlob) => {
+        myImage.src = URL.createObjectURL(myBlob);
+      });
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.dev {
+  height: 100px;
+  width: 100px;
+  background-color: #555;
+}
+</style>
